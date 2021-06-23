914ad695835126a4c62874fb39daa43f66717cf4
Ruairi Conway <hi@ruairiconway.com>
2021-06-23 16:48:58 -0400

    image comp now block display

--- src/components/Image/styles.js
+++ src/components/Image/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 export const Img = styled.img`
+    display: block;
     box-shadow: var(--base-shadow);
     filter: greyscale(100%);
     max-width: 100%;
